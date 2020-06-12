45c865d53f974e01629aadc24c12017f6e22b5dd
Benson Chen <tumlivein@gmail.com>
2020-06-12 10:39:58 +0800

    3. Standard Library & Servers - Read JSON file

--- sandbox.ts
+++ sandbox.ts
@@ -1,5 +1,5 @@
-// uuid module
-import { v4 } from "https://deno.land/std/uuid/mod.ts";
+// fs module
+import { readJson, writeJson } from 'https://deno.land/std/fs/mod.ts';
 
-const uid = v4.generate();
-console.log(uid);
+const jsonObj = await readJson('ninjas.json');
+console.log(jsonObj);
