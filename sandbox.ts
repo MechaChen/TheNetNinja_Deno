10a13180c697d27add149a6c4ecabbc403ba18f9
Benson Chen <tumlivein@gmail.com>
2020-06-12 10:44:33 +0800

    3. Standard Library & Servers - Write JSON file

--- sandbox.ts
+++ sandbox.ts
@@ -3,3 +3,11 @@ import { readJson, writeJson } from 'https://deno.land/std/fs/mod.ts';
 
 const jsonObj = await readJson('ninjas.json');
 console.log(jsonObj);
+
+const books = [
+    { title: 'the way of kings', author: 'brandon sanderson', },
+    { title: 'nameof the wind', author: 'patrick rothfuss' },
+];
+
+await writeJson('books.json', books, { spaces: 2 });
+console.log('created books.json');
