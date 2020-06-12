195eff806f6e9c376a9f08a354328d99f11af00c
Benson Chen <tumlivein@gmail.com>
2020-06-12 10:53:19 +0800

    3. Standard Library & Servers - Respond request and Get request object

--- sandbox.ts
+++ sandbox.ts
@@ -4,5 +4,6 @@ const server = serve({ port: 3000 });
 console.log('listening for requests on port 3000');
 
 for await (const req of server) {
-    console.log('request made');
+    const url = req.url;
+    req.respond({ body: `Hello ninjas, you visited ${url}` });
 }
